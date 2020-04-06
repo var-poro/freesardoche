31a606aff9a149aee2112b5c26b5801f9f4d5b06
PoroWCI <62948870+PoroWCI@users.noreply.github.com>
2020-04-06 16:53:56 +0200

    initial commit
    first version, misses the redirection to twitch.tv/sardoche when count is up

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+import YouTube from 'react-youtube';
+import Clock from './Clock';class App extends Component {     constructor(props) {
+          super(props);
+          this.state = { deadline: 'May, 26, 2020' };
+     }     render() {
+          return (
+              <div className="App">
+                   <div className="App-title">
+                        Sardoche: la libération
+                   </div>
+                   <YouTube className="videoYt" videoId="vKDJfBsiXgw"/>
+                   <div className="App-date">
+                        
+                   </div>                   <Clock deadline={ this.state.deadline }/>
+                   <footer>Made by <a href="https://twitter.com/tv_poro"><button>Poro</button></a></footer>
+              </div>
+          );
+     }
+}
+export default App;
---
+++ src/Clock.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';class Clock extends Component {
+     constructor(props) {
+     super(props);
+     this.state = {
+               days: 0,
+               hours: 0,
+               minutes: 0,
+               seconds: 0,
+          };
+     }     componentWillMount() {
+          this.getTimeUntil(this.props.deadline);
+     }     componentDidMount() {
+          setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+     }       leading0(num) {
+          return num < 10 ? '0' + num : num;
+     }     getTimeUntil(deadline) {
+          const time = Date.parse(deadline) - Date.parse(new Date());
+          if(time < 0) {
+               this.setState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+          
+          } else {
+               const seconds = Math.floor((time/1000)%60);
+               const minutes = Math.floor((time/1000/60)%60);
+               const hours = Math.floor((time/(1000*60*60))%24);
+               const days = Math.floor(time/(1000*60*60*24));
+               this.setState({ days, hours, minutes, seconds });
+          }
+     }     render() {
+          return(
+               <div className="fullDate">
+                   <div className="Days">
+                    <div className="Clock-days">
+                            {this.leading0(this.state.days)} jours
+                        </div>
+                   <img  src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/signup/en_US/89d3e008377d368fc97c63ff92fbee3ee5ae599c/assets/en_US/assets/divider.png"/>
+                   </div>                 <div className="Clock-hours">
+                         {this.leading0(this.state.hours)} heures
+                    </div><div className="vr"></div><div className="Clock-minutes">
+                         {this.leading0(this.state.minutes)} minutes
+                    </div><div className="vr"></div><div className="Clock-seconds">
+                         {this.leading0(this.state.seconds)} secondes
+                    </div>
+               </div>
+          );
+     }
+}
+export default Clock;
